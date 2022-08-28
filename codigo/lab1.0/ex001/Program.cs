26b55f5077e37cbff038586eb4ffa26e48c932c6
Smartucheli <savio.martucheli@gmail.com>
2022-08-28 10:57:31 -0300

    resolução dos exercicios

---
+++ codigo/lab1.0/ex001/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, divnum = 0, resto = 0, soma = 0, quadrado = 0;
+            Console.Write("Digite um numero: ");
+            a = int.Parse(Console.ReadLine());
+            divnum = a / 100;
+            resto = a % 100;
+            soma = divnum + resto;
+            quadrado = soma * soma;
+            Console.WriteLine("A soma de {0} com {1} é igual a {2}", divnum, resto, soma);
+            Console.WriteLine("E o quadrado é {0}", quadrado);
+        }
+    }
+}
