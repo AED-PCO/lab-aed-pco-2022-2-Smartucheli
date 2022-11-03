5bb3c1eb6d0bd7ac3d2799b902cb5fd30f7cbaac
Smartucheli <savio.martucheli@gmail.com>
2022-11-03 11:19:50 -0300

    Atualizando

---
+++ Laboratorios/prova_refeita lab7/questão01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+int l_igual = 0, l_diferente = 0;
+string texto;
+
+Console.Write("Digite um texto: ");
+texto = Console.ReadLine();
+
+var file = File.ReadAllLines("arq1.txt");
+
+while(texto != ""){
+    if(texto == file){
+        Console.WriteLine("igual");
+        l_igual++;
+    } else {
+        Console.Write("Diferente");
+        l_diferente++;
+    }
+}
+
